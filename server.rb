5b882707fd93c58b4d68ef0ac64625b466dc7c53
Carlos Muñoz <karm666@hotmail.com>
2013-09-14 10:31:29 +0200

    primera prueba de conexión a MongoLab

--- server.rb
+++ server.rb
@@ -1,17 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'mongo'
+require 'json'
 
 configure do
   CONNECTION  = Mongo::Connection.from_uri('mongodb://test:test@ds043158.mongolab.com:43158/heroku_app17961664')
-  DB 				  = CONNECTION.db("heroku_app17961664")
+  DB 				  = CONNECTION.db('heroku_app17961664')
   #ARMIES			= DB["armies"]
-  UNITS 			= DB["units"]
+  UNITS 			= DB['units']
   #WEAPONS 		= DB["weapons"]
   #SPECIAL_RULES 	= DB["special_rules"]
 #  set :haml, :format => :html5
 end
 
-get '/units' do
-  @units = UNITS.find
+get '/' do
+  # TODO: si es estático debería servirlo un Apache, etc.
+  send_file File.expand_path('index.html', settings.public_folder)
+end
+
+get '/api/units' do
+  content_type :json
+  @units = UNITS.find.to_a
+  JSON.pretty_generate(@units)
 end
