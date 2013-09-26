2f51451b5bcb019f1a74adefee60c1cd6a00c82b
Carlos Muñoz <karm666@hotmail.com>
2013-09-26 19:45:41 +0200

    CRUD

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ angular.module('army-builder', ['phonecatFilters', 'armyBuilderServices']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when('/units', {templateUrl: 'partials/unit-list.html', controller: UnitListCtrl}).
+            when('/units/new', {templateUrl: 'partials/unit-creation.html', controller: UnitCreationCtrl}).
             when('/units/:unitId', {templateUrl: 'partials/unit-detail.html', controller: UnitDetailCtrl}).
             otherwise({redirectTo: '/units'});
     }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,30 @@
-function UnitListCtrl($scope, Unit) {
+function UnitListCtrl($scope, $location, Unit) {
     $scope.units = Unit.query();
     $scope.orderProp = 'name';
+
+    $scope.newUnit = function() {
+        $location.path('/units/new');
+    };
 }
 
 function UnitDetailCtrl($scope, $routeParams, Unit) {
     $scope.unit = Unit.get({unitId: $routeParams.unitId}, function(unit) {
         $scope.mainImageUrl = unit.images[0];
+
+        $scope.edit = function() {
+            console.log('edit');
+        };
+
+        $scope.delete = function() {
+            console.log('delete');
+        };
+
+
     });
+}
+
+function UnitCreationCtrl($scope, Unit) {
+
 }
 
 
