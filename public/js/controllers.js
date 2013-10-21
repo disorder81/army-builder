07bfa5222878d3e8b39e007d327a3d321bd99d64
Carlos Muñoz <karm666@hotmail.com>
2013-10-21 20:34:28 +0200

    seleccion multiple, core

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -24,6 +24,51 @@ angular.module('armyBuilder')
 
     .controller('UnitCreationCtrl', function($scope, $location, Unit) {
 
+        /*<option value="ar">Artillería</option>
+        <option value="be">Bestia</option>
+        <option value="mo">Moto</option>
+        <option value="cb">Caballería</option>
+        <option value="mre">Moto a reacción Eldar</option>
+        <option value="cm">Criatura monstruosa</option>
+        <option value="cmv">Criatura monstruosa voladora</option>
+        <option value="in">Infantería</option>
+        <option value="ur">Unidad a reacción</option>
+        <option value="mr">Moto a reacción</option>
+        <option value="sa">Unidad de salto</option>
+        <option value="pj">Personaje</option>
+        <option value="vehicle">Vehículo</option> */
+
+        // TODO: Core
+        $scope.types = [
+            {key: "ar", name: "Artilleria"},
+            {key: "be", name: "Bestia"},
+            {key: "mo", name: "Moto"},
+            {key: "cb", name: "Caballería"},
+            {key: "mre", name: "Moto a reacción Eldar"},
+            {key: "cm", name: "Criatura monstruosa"},
+            {key: "cmv", name: "Criatura monstruosa voladora"},
+            {key: "in", name: "Infantería"},
+            {key: "ur", name: "Unidad a reacción"},
+            {key: "mr", name: "Moto a reacción"},
+            {key: "sa", name: "Unidad de salto"},
+            {key: "pj", name: "Personaje"},
+            {key: "vehicle", name: "Vehicle"}];
+
+        /*<input type="radio" name="seccion" value="hq" ng-model="unit.section">CG</input>
+        <input type="radio" name="seccion" value="elite" ng-model="unit.section">Elite</input>
+        <input type="radio" name="seccion" value="line" ng-model="unit.section">Tropas de línea</input>
+        <input type="radio" name="seccion" value="designated-transport" ng-model="unit.section">Transportes asignados</input>
+        <input type="radio" name="seccion" value="fast-attack" ng-model="unit.section">Ataque rápido</input>
+        <input type="radio" name="seccion" value="heavy-support" ng-model="unit.section">Apoyo pesado</input>*/
+
+        $scope.sections = [
+            {key: "hq", name: "CG"},
+            {key: "elite", name: "Elite"},
+            {key: "line", name: "Tropas de línea"},
+            {key: "designated-transport", name: "Transportes asignados"},
+            {key: "fast-attack", name: "Ataque rápido"},
+            {key: "heavy-support", name: "Apoyo pesado"}];
+
         var master = {
             name: 'nombre',
             cost: 0,
