4963813f1a0858fd6ec3868f88b4e133fafa2287
Carlos Muñoz <karm666@hotmail.com>
2013-11-20 19:22:51 +0100

    "core" a un service

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -22,37 +22,10 @@ angular.module('armyBuilder')
          });
     })
 
-    .controller('UnitCreationCtrl', function($scope, $location, Unit) {
+    .controller('UnitCreationCtrl', function($scope, $location, Unit, CoreService) {
 
-        // TODO: Core
-        $scope.types = [
-            {value: "troop", name: "Tropa", subtypes: [
-                {value: "ar", name: "Artilleria"},
-                {value: "be", name: "Bestia"},
-                {value: "mo", name: "Moto"},
-                {value: "cb", name: "Caballería"},
-                {value: "mre", name: "Moto a reacción Eldar"},
-                {value: "cm", name: "Criatura monstruosa"},
-                {value: "cmv", name: "Criatura monstruosa voladora"},
-                {value: "in", name: "Infantería"},
-                {value: "ur", name: "Unidad a reacción"},
-                {value: "mr", name: "Moto a reacción"},
-                {value: "sa", name: "Unidad de salto"},
-                {value: "pj", name: "Personaje"}]},
-            {value: "vehicle", name: "Vehículo", subtypes: [
-                {value: "walker", name: "Andador"},
-                {value: "tank", name: "Tanque"},
-                {value: "flyer", name: "Volador"},
-                {value: "hover", name: "Gravítico"}]}];
-
-        // TODO: Core
-        $scope.sections = [
-            {value: "hq", name: "CG"},
-            {value: "elite", name: "Elite"},
-            {value: "line", name: "Tropas de línea"},
-            {value: "designated-transport", name: "Transportes asignados"},
-            {value: "fast-attack", name: "Ataque rápido"},
-            {value: "heavy-support", name: "Apoyo pesado"}];
+        $scope.sections = CoreService.sections;
+        $scope.types = CoreService.types;
 
         var master = {
             name: 'nombre',
--- public/js/services.js
+++ public/js/services.js
@@ -13,5 +13,40 @@ angular.module('armyBuilderServices', ['ngResource']).
                 query: {method: 'GET', isArray: true},
                 update: {method: 'PUT'}
             });
+    })
+
+
+    .factory('CoreService', function() {
+
+        return {
+            types: [
+                {value: "troop", name: "Tropa", subtypes: [
+                    {value: "ar", name: "Artilleria"},
+                    {value: "be", name: "Bestia"},
+                    {value: "mo", name: "Moto"},
+                    {value: "cb", name: "Caballería"},
+                    {value: "mre", name: "Moto a reacción Eldar"},
+                    {value: "cm", name: "Criatura monstruosa"},
+                    {value: "cmv", name: "Criatura monstruosa voladora"},
+                    {value: "in", name: "Infantería"},
+                    {value: "ur", name: "Unidad a reacción"},
+                    {value: "mr", name: "Moto a reacción"},
+                    {value: "sa", name: "Unidad de salto"},
+                    {value: "pj", name: "Personaje"}]},
+                {value: "vehicle", name: "Vehículo", subtypes: [
+                    {value: "walker", name: "Andador"},
+                    {value: "tank", name: "Tanque"},
+                    {value: "flyer", name: "Volador"},
+                    {value: "hover", name: "Gravítico"}]}],
+
+            sections: [
+                {value: "hq", name: "CG"},
+                {value: "elite", name: "Elite"},
+                {value: "line", name: "Tropas de línea"},
+                {value: "designated-transport", name: "Transportes asignados"},
+                {value: "fast-attack", name: "Ataque rápido"},
+                {value: "heavy-support", name: "Apoyo pesado"}]
+
+        }
     });
 
