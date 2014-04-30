93a445a78eaef5c476dc110f8fcefd77ff58beb9
Carlos Muñoz <karm666@hotmail.com>
2014-04-30 14:51:38 +0200

    varios refactor de creación de unidades

--- public/js/services.js
+++ public/js/services.js
@@ -67,78 +67,79 @@ angular.module('armyBuilderServices', ['ngResource']).
     })
 
 
-    .service('CoreService', function() {
+    .service(
+        'CoreService',
+        function() {
+            var types = [
+                    {value: "troop", name: "Tropa", subtypes: [
+                        {value: "ar", name: "Artilleria"},
+                        {value: "be", name: "Bestia"},
+                        {value: "mo", name: "Moto"},
+                        {value: "cb", name: "Caballería"},
+                        {value: "mre", name: "Moto a reacción Eldar"},
+                        {value: "cm", name: "Criatura monstruosa"},
+                        {value: "cmv", name: "Criatura monstruosa voladora"},
+                        {value: "in", name: "Infantería"},
+                        {value: "ur", name: "Unidad a reacción"},
+                        {value: "mr", name: "Moto a reacción"},
+                        {value: "sa", name: "Unidad de salto"},
+                        {value: "pj", name: "Personaje"}
+                    ]},
+                    {value: "vehicle", name: "Vehículo", subtypes: [
+                        {value: "walker", name: "Andador"},
+                        {value: "tank", name: "Tanque"},
+                        {value: "flyer", name: "Volador"},
+                        {value: "hover", name: "Gravítico"}
+                    ]}
+                ],
 
-        var types = [
-                {value: "troop", name: "Tropa", subtypes: [
-                    {value: "ar", name: "Artilleria"},
-                    {value: "be", name: "Bestia"},
-                    {value: "mo", name: "Moto"},
-                    {value: "cb", name: "Caballería"},
-                    {value: "mre", name: "Moto a reacción Eldar"},
-                    {value: "cm", name: "Criatura monstruosa"},
-                    {value: "cmv", name: "Criatura monstruosa voladora"},
-                    {value: "in", name: "Infantería"},
-                    {value: "ur", name: "Unidad a reacción"},
-                    {value: "mr", name: "Moto a reacción"},
-                    {value: "sa", name: "Unidad de salto"},
-                    {value: "pj", name: "Personaje"}
-                ]},
-                {value: "vehicle", name: "Vehículo", subtypes: [
-                    {value: "walker", name: "Andador"},
-                    {value: "tank", name: "Tanque"},
-                    {value: "flyer", name: "Volador"},
-                    {value: "hover", name: "Gravítico"}
-                ]}
-            ],
+                sections = [
+                    {value: "hq", name: "CG"},
+                    {value: "elite", name: "Elite"},
+                    {value: "line", name: "Tropas de línea"},
+                    {value: "designated-transport", name: "Transportes asignados"},
+                    {value: "fast-attack", name: "Ataque rápido"},
+                    {value: "heavy-support", name: "Apoyo pesado"}
+                ],
 
-            sections = [
-                {value: "hq", name: "CG"},
-                {value: "elite", name: "Elite"},
-                {value: "line", name: "Tropas de línea"},
-                {value: "designated-transport", name: "Transportes asignados"},
-                {value: "fast-attack", name: "Ataque rápido"},
-                {value: "heavy-support", name: "Apoyo pesado"}
-            ],
-
-            unit = {
-                name: 'Nombre',
-                army: {},
-                cost: 0,
-                description: '<p>Descripcion!</p>',
-                section: sections[0].value,
-                type: types[0].value,
-                subtypes: [],
-                unique: false,
-                composition: {
-                    min: 1,
-                    max: 3
+                unit = {
+                    name: 'Nombre',
+                    army: {},
+                    cost: 0,
+                    description: '<p>Descripcion!</p>',
+                    section: sections[0].value,
+                    type: types[0].value,
+                    subtypes: [],
+                    unique: false,
+                    composition: {
+                        min: 1,
+                        max: 3
+                    },
+                    stats: {
+                        ws: 3,
+                        bs: 3,
+                        s: 3,
+                        t: 3,
+                        w: 3,
+                        i: 3,
+                        a: 3,
+                        ld: 3,
+                        sv: 3
+                    },
+                    wargear: [],
+                    specialRules: [],
+                    options: []
                 },
-                stats: {
-                    ws: 3,
-                    bs: 3,
-                    s: 3,
-                    t: 3,
-                    w: 3,
-                    i: 3,
-                    a: 3,
-                    ld: 3,
-                    sv: 3
-                },
-                wargear: [],
-                specialRules: [],
-                options: []
-            },
 
-            rule = {
-                types: ['universal', 'army', 'faction', 'individual']
-            };
+                rule = {
+                    types: ['universal', 'army', 'faction', 'individual']
+                };
 
-        return {
-            types: types,
-            sections: sections,
-            unit: unit
-        }
+            return {
+                types: types,
+                sections: sections,
+                unit: unit
+            }
     })
 
     .service('ArmyService', function($q, $log, Army, Unit) {
