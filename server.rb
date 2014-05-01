9e58c918c8b0c3efb64d45b08ab33f206b59448a
Carlos Muñoz <karm666@hotmail.com>
2014-05-01 13:23:08 +0200

    heroku

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ require 'json'
 #set :environment => 'local'
 
 
-=begin
+
 configure do
   CONNECTION  = Mongo::Connection.from_uri('mongodb://test:test@ds043158.mongolab.com:43158/heroku_app17961664')
   DATABASE 		= CONNECTION.db('heroku_app17961664')
@@ -16,9 +16,9 @@ configure do
   RULES       = DATABASE['rules']
 #  set :haml, :format => :html5
 end
-=end
 
 
+=begin
 # RACK_ENV=local
 configure do
   #system('mongod --dbpath c:\dev\data\mongodb')
@@ -29,6 +29,7 @@ configure do
   WEAPONS     = DATABASE['weapons']
   RULES       = DATABASE['rules']
 end
+=end
 
 helpers do
 
